d792e8bfafedb958e1bf859bdd83179f08558b5f
Adams Unity <128307592+AdamsUnity@users.noreply.github.com>
2024-01-18 18:23:19 +0100

    first commit

---
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import Item from "../Item/Item";
+import "./NewCollections.css";
+const NewCollections = () => {
+  const [new_collection, setNew_Collcetion] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/newcollections")
+      .then((response) => response.json())
+      .then((data) => setNew_Collcetion(data));
+  }, []);
+  return (
+    <div className="new-collections">
+      <h1>NEW COLLECTIONS</h1>
+      <hr />
+      <div className="collections">
+        {new_collection.map((item, idx) => {
+          return (
+            <Item
+              key={idx}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default NewCollections;
